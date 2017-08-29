fabe30491fc429c0c25025a27a98acadbfc0ff00
zubintang <zubintang@qq.com>
2017-08-29 14:55:24 +0800

    Update RNExitAppPackage.java
    适配RN 0.47

--- android/src/main/java/com/github/wumke/RNExitApp/RNExitAppPackage.java
+++ android/src/main/java/com/github/wumke/RNExitApp/RNExitAppPackage.java
@@ -1,6 +1,5 @@
 package com.github.wumke.RNExitApp;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -9,7 +8,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class RNExitAppPackage implements ReactPackage {
 
@@ -21,12 +19,6 @@ public class RNExitAppPackage implements ReactPackage {
         modules.add(new RNExitAppModule(reactContext));
 
         return modules;
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-
-        return Collections.emptyList();
     }
 
     @Override
